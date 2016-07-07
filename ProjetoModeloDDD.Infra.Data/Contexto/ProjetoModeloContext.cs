cb8e4b898f7b32e484259e8625654e7597144827
elcalando <reato.neto@gmail.com>
2016-07-06 22:56:55 -0300

    Criado o ViewModel

--- ProjetoModeloDDD.Infra.Data/Contexto/ProjetoModeloContext.cs
+++ ProjetoModeloDDD.Infra.Data/Contexto/ProjetoModeloContext.cs
@@ -19,6 +19,7 @@ namespace ProjetoModeloDDD.Infra.Data.Contexto
         }
 
         public DbSet<Cliente> Clientes {get;set;}
+        public DbSet<Produto> Produtos { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
@@ -37,6 +38,7 @@ namespace ProjetoModeloDDD.Infra.Data.Contexto
                 .Configure(p => p.HasMaxLength(100));
 
             modelBuilder.Configurations.Add(new ClienteConfiguration());
+            modelBuilder.Configurations.Add(new ProdutoConfiguration());
         }
 
         public override int SaveChanges()
